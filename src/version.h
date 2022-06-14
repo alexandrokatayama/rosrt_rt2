b14822253b3f578e1b0640358ecb7f5c81580bc9
alexandrokatayama <katayama.takahiro@rtworks.co.jp>
2022-06-15 00:01:49 +0900

    Version 1.8.4

--- src/version.h
+++ src/version.h
@@ -4,13 +4,13 @@
  *
  * @author		片山貴寛<katayama.takahiro@rtworks.co.jp>
  * @par			Copyright
- * 2012-2022ce	RT.Works co., ltd. All rights reserved.
+ * 2012-2022	RT.Works co., ltd. All rights reserved.
  *
  *****************************************************************************/
 
 #ifndef _INCLUDE_VERSION_H_
 #define _INCLUDE_VERSION_H_
 
-#define VERSION_NUMBER "1.8.3"
+#define VERSION_NUMBER "1.8.4"
 
 #endif /* _INCLUDE_VERSION_H_ */
